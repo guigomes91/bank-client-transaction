790153d811928c747009027a5d8a7953a9e769bd
guigomes91 <guilherme.gomes91@outlook.com>
2024-02-17 12:08:52 -0300

    first commit

---
+++ src/main/java/br/com/gomes/bankclienttransaction/BankClientTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.gomes.bankclienttransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankClientTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankClientTransactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gomes/bankclienttransaction/BankClientTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gomes.bankclienttransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankClientTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
